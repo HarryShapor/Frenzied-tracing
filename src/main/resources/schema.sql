b4d4b9f7f2f3a00adc02b668ca0bd40ae04d81e7
HarryShapor <Hero1World23@gmail.com>
2026-04-23 22:36:47 +0300

    updating schema db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,29 +3,42 @@ CREATE TABLE boards (
 	height REAL NOT NULL,
 	width REAL NOT NULL,
 	grid_pitch REAL NOT NULL,
-	la
+	layers INT NOT NULL DEFAULT 1,
+	diagonals Boolean NOT NULL DEFAULT false,
+	count_vertical_points INT,
+	count_horizontal_points INT,
+	n INT
+);
 
-)
 
-
--- Таблица с битовыми масками
 CREATE TABLE paths_bitmask (
     id BIGSERIAL PRIMARY KEY,
+
+    vertex_mask BYTEA NOT NULL,
     
-    -- Битовые маски (25 бит для платы 5x5x1)
-    vertex_mask BIT(25) NOT NULL,           -- Какие вершины в пути
+    start_vertex INt NOT NULL,     
+    end_vertex INT NOT NULL,      
+    path_length INT NOT NULL,         
     
-    -- Дополнительная информация
-    start_vertex INt NOT NULL,          -- 0-24
-    end_vertex INT NOT NULL,            -- 0-24
-    path_length INT NOT NULL,           -- Количество вершин
-    
-    CHECK (start_vertex BETWEEN 0 AND 24),
-    CHECK (end_vertex BETWEEN 0 AND 24),
     CHECK (path_length >= 2)
 );
 
--- Индексы для быстрого поиска
 CREATE INDEX idx_start_end ON paths_bitmask (start_vertex, end_vertex);
 CREATE INDEX idx_mask_brin ON paths_bitmask USING brin (vertex_mask);
 CREATE INDEX idx_length ON paths_bitmask (path_length);
+
+
+CREATE TABLE paths_string (
+    id BIGSERIAL PRIMARY KEY,
+    path_string TEXT NOT NULL,
+    start_vertex INT NOT NULL,
+    end_vertex INT NOT NULL,
+    path_length INT NOT NULL,
+	turns INT,
+
+    CHECK (path_length >= 2),
+    CHECK (path_string ~ '^(\d+,)*\d+$')  -- Проверка формата "1,2,3,4"
+);
+
+CREATE INDEX idx_start_end ON paths_string(start_vertex, end_vertex);
+CREATE INDEX idx_path_length ON paths_string(path_length);
